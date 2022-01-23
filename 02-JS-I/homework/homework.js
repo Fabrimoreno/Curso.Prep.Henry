30a6165073b1b467fd9d219673e7334984a95cf6
Fabrimoreno <fafomoreno18@live.com>
2022-01-23 01:48:05 -0300

    firts commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -245,22 +245,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.length != 1 ) {
-    return "Dato incorrecto";
-  }
-    if (letra = "a") {
-    return "Es vocal";
-  }   if (letra = "e") {
-    return "Es vocal";
-    if (letra = "i") {
-      return "Es vocal";
-      if (letra = "o") {
-        return "Es vocal";
-        if (letra = "u") {
-          return "Es vocal";
-          return "Dato incorrecto";
-
-} } } } } 
+}
 
 
 
