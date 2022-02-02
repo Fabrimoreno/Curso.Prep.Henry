61ec8626acdaaae9eff9d2efe350edcfcf75261a
Fabrimoreno <fafomoreno18@live.com>
2022-02-02 17:54:20 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -122,6 +122,15 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if ((numero%3) === 0 && (numero%5) === 0) {
+    return "fizz buzz";
+  } else if ((numero%3) === 0) {
+    return "fizz";
+  } else if ((numero%5) === 0) {
+    return "buzz";
+  } else {
+    return numero;
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
